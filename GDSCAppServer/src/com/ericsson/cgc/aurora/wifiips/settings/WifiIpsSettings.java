7e8d0fab55918299b88226fa6ebd0b5af6574e70
Hoare <hoarewu@qq.com>
2014-01-21 13:49:53 +0800

    A02: update log file path and deployment name as 'GDSCAppServer'.

--- GDSCAppServer/src/com/ericsson/cgc/aurora/wifiips/settings/WifiIpsSettings.java
+++ GDSCAppServer/src/com/ericsson/cgc/aurora/wifiips/settings/WifiIpsSettings.java
@@ -3,7 +3,7 @@ package com.ericsson.cgc.aurora.wifiips.settings;
 public class WifiIpsSettings {
 	public static boolean DEBUG = true;
 	public static boolean DBM_LINEAR_AVERAGE = true;
-	public static String LOGGER_FILE = "C:\\Users\\eshouwu\\Projects\\Aurora Project\\PoC\\IPSWifiIPSServer.log"; //"\\\\ev2c4138b15c2b\\A_Paddy\\newdev\\wifiipsserver.log";
+	public static String LOGGER_FILE = "C:\\IPSServer.log"; //"\\\\ev2c4138b15c2b\\A_Paddy\\newdev\\wifiipsserver.log";
 	public static String Database_Server = "localhost";
 	public static float pilot_power_threshold = 15.0f;
 	public static float mse_threshold = 1.0f; // 0.01 is too small???
